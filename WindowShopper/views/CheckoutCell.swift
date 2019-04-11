9b9423db0f2f7cfcecf02a201a75d65bc1ec090f
Jovel Madrid <jovelmadrid@gmail.com>
2019-04-10 21:58:32 -0700

    Refactored Code
    Removed unneccesary labels and views. Refactored code to use Auto Layout along with changing colors of specific views.

--- WindowShopper/views/CheckoutCell.swift
+++ WindowShopper/views/CheckoutCell.swift
@@ -14,13 +14,9 @@ class CheckoutCell: UITableViewCell {
         
         mItemTitle.font = UIFont.boldSystemFont(ofSize: 17)
         
-        mQuantityLabel.frame = CGRect(x: 46, y: 27, width: 99, height: 21)
         mQuantityLabel.font = UIFont.systemFont(ofSize: 17)
+        mQuantityStepper.tintColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
         
-        mQuantityStepper.frame = CGRect(x: 177, y: 23, width: 94, height: 29)
-        mQuantityStepper.tintColor = #colorLiteral(red: 0.849943329, green: 0.7401361611, blue: 0.239743404, alpha: 1)
-        
-        mPriceLabel.frame = CGRect(x: 325, y: 26, width: 82, height: 21)
         mPriceLabel.font = UIFont.systemFont(ofSize: 17)
     }    
 
