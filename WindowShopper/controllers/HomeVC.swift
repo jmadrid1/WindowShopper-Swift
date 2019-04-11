ca5731b9093205abedf6daf92581608161c8d777
Jovel Madrid <jovelmadrid@gmail.com>
2019-04-10 21:57:47 -0700

    Updated UI and Refactored Code
    Replaced the assets associated with navigating to the Mens' and Womens' clothing sections along with refactoring to utilize AutoLayout

--- WindowShopper/controllers/HomeVC.swift
+++ WindowShopper/controllers/HomeVC.swift
@@ -12,16 +12,11 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        mBannerView.frame = CGRect(x: 0, y: 64, width: 414, height: 94)
         mBannerView.backgroundColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
         
         mMensPanelImage.image = UIImage(named: "mens_panel.png")
         mWomensPanelImage.image = UIImage(named: "womens_panel.png")
-        
-        mMensPanelImage.frame = CGRect(x: 0, y: 171, width: 212, height: 462)
-        
-        mWomensPanelImage.frame = CGRect(x: 227, y: 171, width: 187, height: 462)
-        
+
         mMensPanelImage.isUserInteractionEnabled = true
         mWomensPanelImage.isUserInteractionEnabled = true
         
