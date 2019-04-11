0a9d32df4b02e5a128e65998c5a88c729e511308
Jovel Madrid <jovelmadrid@gmail.com>
2019-04-10 21:55:21 -0700

    Updated UI and Refactored Code
    Removed unneccesary labels and views.  Updated UI and refactored code to use Auto Layout along with changing colors of specific views.

--- WindowShopper/controllers/UserLoginVC.swift
+++ WindowShopper/controllers/UserLoginVC.swift
@@ -6,9 +6,6 @@ class UserLoginVC: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var mLogoImage: UIImageView!
     
-    @IBOutlet weak var mEmailLabel: UILabel!
-    @IBOutlet weak var mPasswordLabel: UILabel!
-    
     @IBOutlet weak var mEmailTextField: UITextField!
     @IBOutlet weak var mPasswordTextField: UITextField!
     
@@ -21,35 +18,28 @@ class UserLoginVC: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
 
         mLogoImage.image =  UIImage(named: "ic_windowshopper_transparent.png")
-        mLogoImage.frame = CGRect(x: 155, y: 174, width: 133, height: 128)
-        
-        mEmailLabel.text = "Email:"
-        mPasswordLabel.text = "Password:"
         
         mEmailTextField.tag = 0
         mPasswordTextField.tag = 1
         
-        mEmailTextField.frame = CGRect(x: 120, y: 324, width: 223, height: 30)
-        mPasswordTextField.frame = CGRect(x: 120, y: 370, width: 223, height: 30)
-        
         mEmailTextField.keyboardType = UIKeyboardType.emailAddress
         mPasswordTextField.isSecureTextEntry = true
         
-        mLoginButton.setTitle("Login", for: .normal)
-        mLoginButton.setTitleColor( #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+    
         mLoginButton.titleLabel?.font = UIFont.systemFont(ofSize: 19)
-        mLoginButton.frame = CGRect(x: 196, y: 417, width: 49, height: 30)
+        mLoginButton.backgroundColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+        mLoginButton.layer.cornerRadius = 15
+        mLoginButton.layer.borderWidth = 2
+        mLoginButton.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
         mIncorrectPasswordLabel.text = "*Email and/or password do not match any accounts"
-        mIncorrectPasswordLabel.textColor = UIColor.red
+        mIncorrectPasswordLabel.textColor = UIColor.darkGray
         mIncorrectPasswordLabel.font = UIFont.systemFont(ofSize: 15)
-        mIncorrectPasswordLabel.frame = CGRect(x: 20, y: 455, width: 385, height: 21)
         mIncorrectPasswordLabel.isHidden = true
         
         mCreateAccountButton.setTitle("Create An Account", for: .normal)
-        mCreateAccountButton.setTitleColor( #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
-        mCreateAccountButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-        mCreateAccountButton.frame = CGRect(x: 143, y: 642, width: 143, height: 30)
+//        mCreateAccountButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+//        mCreateAccountButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
     }
     
     override func didReceiveMemoryWarning() {
